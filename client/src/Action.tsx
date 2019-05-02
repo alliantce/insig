7ae8e46cbc6f4f4ce959f43706df6d8e35d301a2
Bernardo Vieira <bernardo5@live.com.pt>
2019-05-02 17:38:06 +0100

    finish action page

--- client/src/Action.tsx
+++ client/src/Action.tsx
@@ -3,17 +3,48 @@ import React, { Component } from 'react';
 import './main.scss';
 import './action.scss';
 
-class Action extends Component {
+interface IActionState {
+    action: string;
+    listActions: string [];
+}
+class Action extends Component<{}, IActionState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            action: '',
+            listActions: [],
+        }
+    }
+
+    componentDidMount() {
+        this.loadActions();
+    }
+
+    handleChange = (event: any) => {
+        this.setState({ action: event.target.value });
+    }
+
+    handleSubmit = (event: any) => {
+        const { action } = this.state;
+        alert('hi, value '+ action);
+        event.preventDefault();
+    }
+
+    loadActions() {
+        const someRandomValues = ['Unordered list item 1', 'Unordered list item 2'];
+        this.setState({listActions: someRandomValues});
+    }
+
     render() {
+        const { action, listActions } = this.state;
         return (
             <main>
-                <form>
-                    <input type="text" name="action" />
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="action" value={action} onChange={this.handleChange} placeholder="Action message" />
                     <input type="submit" />
                 </form>
                 <ul>
-                    <li>Unordered list item 1</li>
-                    <li>Unordered list item 2</li>
+                    {listActions.map(e => <li key={e}>{e}</li>)}
                 </ul>
             </main>
         );
