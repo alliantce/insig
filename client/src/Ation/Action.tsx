6056f429a4fa8e929bfccf8f5eaf6169c955d3bd
Bernardo Vieira <bernardo5@live.com.pt>
2019-05-09 16:57:19 +0100

    add and get actions

--- client/src/Ation/Action.tsx
+++ client/src/Ation/Action.tsx
@@ -1,23 +1,66 @@
 import React, { Component } from 'react';
 
+import { createLogger, format, transports } from 'winston';
+import BlockchainGeneric from '../Common/BlockchainGeneric';
+import { IBlockchainState, ISupplyChain } from '../Common/CommonInterfaces';
 import '../main.scss';
 import './action.scss';
 
-interface IActionState {
+/**
+ * Setting up a logger.
+ */
+const logger = createLogger({
+    format: format.combine(
+        format.timestamp(),
+        format.json(),
+    ),
+    level: 'debug',
+    transports: [
+        new transports.Console(),
+    ],
+});
+/**
+ * Define class interface
+ */
+interface IActionState extends IBlockchainState {
     action: string;
     listActions: string [];
+    supplyChain: ISupplyChain;
 }
+/**
+ * Action class
+ */
 class Action extends Component<{}, IActionState> {
+    /**
+     * @ignore
+     */
     constructor(props: any) {
         super(props);
         this.state = {
             action: '',
             listActions: [],
+            supplyChain: undefined as any,
+            userAccount: undefined as any,
+            web3: undefined as any,
         };
     }
 
+    /**
+     * @ignore
+     */
     public componentDidMount() {
-        this.loadActions();
+        logger.info('[START] componentDidMount');
+        BlockchainGeneric.onLoad().then((generic) => {
+            BlockchainGeneric.loadSupplyChain(generic.web3).then((contracts) => {
+                this.setState({
+                    supplyChain: contracts.supplyChain,
+                    userAccount: generic.userAccount,
+                    web3: generic.web3,
+                });
+                this.loadActions().then((actionsName) => this.setState({listActions: actionsName}));
+            });
+        });
+        logger.info('[END] componentDidMount');
     }
 
     public handleChange = (event: any) => {
@@ -25,16 +68,14 @@ class Action extends Component<{}, IActionState> {
     }
 
     public handleSubmit = (event: any) => {
-        const { action } = this.state;
-        alert('hi, value ' + action);
+        const { action, supplyChain, userAccount } = this.state;
+        supplyChain.addAction(action, { from: userAccount }).then(() => alert('Success!'));
         event.preventDefault();
     }
 
-    public loadActions() {
-        const someRandomValues = ['Unordered list item 1', 'Unordered list item 2'];
-        this.setState({listActions: someRandomValues});
-    }
-
+    /**
+     * @ignore
+     */
     public render() {
         const { action, listActions } = this.state;
         return (
@@ -54,6 +95,16 @@ class Action extends Component<{}, IActionState> {
                 </ul>
             </main>
         );
+    }
+
+    private async loadActions() {
+        const { supplyChain } = this.state;
+        const actionsName: string[] = [];
+        const totalActions = await supplyChain.totalActions();
+        for (let a = 1; a <= totalActions; a += 1) {
+            actionsName.push(await supplyChain.actionDescription(a));
+        }
+        return actionsName;
     }
 }
 
